be1e9ff965b0b12ec7a3baa117ecf4ddc1d1110a
Maa8nya <maanya.s.aithal@gmail.com>
2025-07-04 17:24:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Homepage from './components/Homepage';
+import HeroSection from './components/HeroSection';
+import ServicesCarousel from './components/ServicesCarousel';
+import ResearchReports from './components/ResearchReports';
+import TestimonialsSection from './components/TestimonialsSection';
+import ScrollRevealHero from './components/ScrollRevealHero';
 import ContactSection from "./components/ContactSection";
 import CollaborationSection from "./components/CollaborationSection";
 
@@ -9,6 +15,12 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Homepage />
+          <HeroSection />
+          <ServicesCarousel />
+          <ResearchReports />
+          <TestimonialsSection />
+          <ScrollRevealHero />
           <Route path="/contact" element={<ContactSection />} />
           <Route path="/collaboration" element={<CollaborationSection />} />
         </Routes>
