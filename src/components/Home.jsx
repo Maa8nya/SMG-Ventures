97a5d516abf03c30afb99d084ec1671df82a1382
Maa8nya <maanya.s.aithal@gmail.com>
2025-07-04 18:44:54 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import Intro from "./Intro"; // your animated intro component
 import HomeContent from "./HomeContent";
+import Homepage from './components/Homepage';
+import HeroSection from './components/HeroSection';
+import ServicesCarousel from './components/ServicesCarousel';
+import ResearchReports from './components/ResearchReports';
+import TestimonialsSection from './components/TestimonialsSection';
+import ScrollRevealHero from './components/ScrollRevealHero';
+import ContactSection from "./components/ContactSection";
+import ResponseCard from "./components/ResponseCard";
+import CollaborationSection from "./components/CollaborationSection";
 
 export default function HomePage() {
   const [showMainContent, setShowMainContent] = useState(false);
@@ -17,6 +26,7 @@ export default function HomePage() {
           <ResearchReports />
           <TestimonialsSection />
           <ScrollRevealHero />
+      <ResponseCard />
       )}
     </>
   );
