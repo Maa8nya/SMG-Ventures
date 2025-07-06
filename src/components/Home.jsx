8ce307aa4a4c00861cdc48aff9ea4b58d1161e0c
Ramya-cmd676 <2022cs_ramyakm_c@nie.ac.in>
2025-07-07 00:06:15 +0530

    deleted unwanted components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,11 @@
 import { useState } from "react";
 import Intro from "./Intro"; // your animated intro component
 import HomeContent from "./HomeContent";
-import Homepage from './Homepage';
 import HeroSection from './HeroSection';
 import ServicesCarousel from './ServicesCarousel';
 import ResearchReports from './ResearchReports';
-import TestimonialsSection from './TestimonialsSection'; 
-import ScrollRevealHero from './ScrollRevealHero';
 import Footer from './Footer';
-import ContactSection from "./ContactSection";
-import ResponseCard from "./ResponseCard";
 import WhySMGVentures from "./WhySMGVentures";
-import CollaborationSection from "./CollaborationSection";
 
 export default function HomePage() {
   const [showMainContent, setShowMainContent] = useState(false);
@@ -23,13 +17,10 @@ export default function HomePage() {
       ) : (
         <div>
           <HomeContent />
-        <Homepage />
           <HeroSection />
           <ServicesCarousel />
           <ResearchReports />
-          <TestimonialsSection />
           <WhySMGVentures/>
-          <ScrollRevealHero />
       <Footer />
         </div>
        
